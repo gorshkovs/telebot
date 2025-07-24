19d069d4a8b20c99f430b2f745bb3da2ae1a970e
gorshkovs <93306011+gorshkovs@users.noreply.github.com>
2025-07-24 16:27:39 +0600

    main.py

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ def send_product_info(chat_id, product_code, reply_to_message_id):
     description = ""
     
    keyboard_contact = types.InlineKeyboardMarkup()
-    keyboard_contact.add(types.InlineKeyboardButton("📨 Написать", url="https://t.me/shyctruk"))
+   keyboard_contact.add(types.InlineKeyboardButton("📨 Написать", url="https://t.me/shyctruk"))
 
     # Ваша логика для загрузки фото и описания в зависимости от product_code
     if product_code == 'DRIS2301IN':
@@ -427,7 +427,7 @@ def send_product_info(chat_id, product_code, reply_to_message_id):
         send_message_unified(
             chat_id=chat_id,
             text=description,
-            reply_markup=keyboard_contact,
+#            reply_markup=keyboard_contact,
             parse_mode="Markdown",
             reply_to_message_id=reply_to_message_id
         )
